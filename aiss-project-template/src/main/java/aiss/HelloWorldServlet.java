d9ee818a5f5fdaac96c2392c2681a22275feb764
ManuErCaximba <ercaximbero96@gmail.com>
2018-07-09 18:23:01 +0200

    prueba commit

--- aiss-project-template/src/main/java/aiss/HelloWorldServlet.java
+++ aiss-project-template/src/main/java/aiss/HelloWorldServlet.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//pene
 public class HelloWorldServlet extends HttpServlet {
 	
 	private static final Logger log = Logger.getLogger(HelloWorldServlet.class.getName());
